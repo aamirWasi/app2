f065b73ba07055eec2f72d5c60e7f6d7f738a760
aamirWasi <46648085+aamirWasi@users.noreply.github.com>
2019-01-17 23:36:22 +0600

    Update Program.cs
    feature3

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -13,7 +13,8 @@ namespace ConsoleApp1
             Console.WriteLine("Hello From the Out Side");
             Console.WriteLine("Sung by adele");//thanks adele
             Console.WriteLine("Wow nice song");//nice song
-            //code from Feature's class
+            //code from Feature1  class
+            //code fromfeature3 class
         }
     }
 }
