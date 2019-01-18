f2dca3eae66d23fb424917b7a96e732e39bf70ad
aamir <wasi3aamir@gmail.com>
2019-01-18 17:35:28 +0600

    problem solve

---
+++ App3/App3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your testcase :");
+            int T = Convert.ToInt32(Console.ReadLine());
+            while (T!=0)
+            {
+                Console.Write("Enter velocity & Time");
+                double velo = Convert.ToDouble(Console.ReadLine());
+                double Time = Convert.ToDouble(Console.ReadLine());
+
+                double deacltor = -(velo / Time);
+                Console.WriteLine("Result is :{0}", deacltor);
+            }
+        }
+    }
+}
---
+++ CodeC1/CodeC1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeC1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0; i < args.Length; i++)
+            {
+
+                Console.WriteLine("Hello World");
+            }
+        }
+    }
+}
